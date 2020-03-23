4270cfd18e8cf536f58667238c7fccdc88674d91
apple0819 <apple0819@users.noreply.github.com>
2020-03-23 17:55:25 +0900

    사용자 정보 실제로 받아와서 출력

--- app/src/main/java/com/example/loginandsignup/MainActivity.java
+++ app/src/main/java/com/example/loginandsignup/MainActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.example.loginandsignup.databinding.ActivityMainBinding;
+import com.example.loginandsignup.datas.User;
 import com.example.loginandsignup.utils.ServerUtil;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends BaseActivity {
@@ -33,6 +35,28 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onResponse(JSONObject json) {
                 Log.d("내정보", json.toString());
+
+                try {
+                    int code = json.getInt("code");
+
+                    if (code == 200) {
+                        JSONObject data = json.getJSONObject("data");
+                        JSONObject user = data.getJSONObject("user");
+
+                        final User myInfo = User.getUserFromJson(user);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                binding.nameTxt.setText(myInfo.getName());
+                                binding.memoTxt.setText(myInfo.getMemo());
+                                binding.phoneTxt.setText(myInfo.getPhone());
+                            }
+                        });
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
