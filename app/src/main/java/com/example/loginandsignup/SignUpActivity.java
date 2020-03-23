ca8d3d0db4ee0a1819003e98b5522e7c7889b3eb
apple0819 <apple0819@users.noreply.github.com>
2020-03-23 15:50:01 +0900

    회원가입 서버 전달까지.  => DB에서 가입 성공 확인  => UI반영 안됨

--- app/src/main/java/com/example/loginandsignup/SignUpActivity.java
+++ app/src/main/java/com/example/loginandsignup/SignUpActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.loginandsignup.databinding.ActivitySignUpBinding;
+import com.example.loginandsignup.utils.ServerUtil;
+
+import org.json.JSONObject;
 
 public class SignUpActivity extends BaseActivity {
 
@@ -23,6 +28,26 @@ public class SignUpActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                ID / 비번 / 이름 / 폰번을 따와서 => 서버로 전달
+//                회원가입 API에 전달
+                String inputId = binding.idEdt.getText().toString();
+                String inputPw = binding.pwEdt.getText().toString();
+                String inputName = binding.nameEdt.getText().toString();
+                String inputPhone = binding.phoneEdt.getText().toString();
+
+                ServerUtil.putRequestSignUp(mContext, inputId, inputPw, inputName, inputPhone, new ServerUtil.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+                        Log.d("회원가입", json.toString());
+                    }
+                });
+
+            }
+        });
 
     }
 
