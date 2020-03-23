6795f8dc2f9f39f90097c5243f5c1b89e332ca03
apple0819 <apple0819@users.noreply.github.com>
2020-03-23 13:46:31 +0900

    로그인 실패시 토스트 출력  - runOnUiThread메쏘드를 이용해서 출력  - 안그러면 앱이 죽는다.

--- app/src/main/java/com/example/loginandsignup/MainActivity.java
+++ app/src/main/java/com/example/loginandsignup/MainActivity.java
@@ -7,14 +7,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import com.example.loginandsignup.databinding.ActivityMainBinding;
+import com.example.loginandsignup.databinding.ActivityMainBindingImpl;
 import com.example.loginandsignup.utils.ContextUtil;
 import com.example.loginandsignup.utils.ServerUtil;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends BaseActivity {
 
-    ActivityMainBinding binding = null;
+    ActivityMainBindingImpl binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +36,31 @@ public class MainActivity extends BaseActivity {
 
 //        체크박스에 체크가 될때 (변화가 있을때) 마다
 //        체크 여부를 저장.
+
         binding.idCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                ContextUtil을 이용해서, 체크 여부 저장.
+
+//                ContextUtil을 이용해서, 체크 여부를 저장.
                 ContextUtil.setIdCheck(mContext, isChecked);
+
             }
         });
 
-//        로그인 버튼을 누르면 => 아이디 저장이 체크되어 있다면 => 입력되어있는 이메일 저장 =>
-//        그렇지 않다면 => 이메일을 빈칸 ""으로 저장
+
+//        로그인 버튼을 누르면 => 아이디 저장이 체크되어 있다면
+//        => 입력되어있는 이메일 저장
+//        그렇지 않다면 => 이메일을 빈칸 "" 으로 저장
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 //                체크박스에 체크가 되어있나?
                 if (binding.idCheckBox.isChecked()) {
 
-//                    체크가 되어있는 상황
+//                    체크가 되어 있는 상황
                     String inputEmail = binding.emailEdt.getText().toString();
+
                     ContextUtil.setEmail(mContext, inputEmail);
                 }
                 else {
@@ -56,11 +68,59 @@ public class MainActivity extends BaseActivity {
                     ContextUtil.setEmail(mContext, "");
                 }
 
+
                 String inputEmail = binding.emailEdt.getText().toString();
                 String inputPw = binding.pwEdt.getText().toString();
-                ServerUtil.postRequestLogin(mContext, inputEmail, inputPw, null);
+
+                ServerUtil.postRequestLogin(mContext, inputEmail, inputPw, new ServerUtil.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+
+//                        응답실행코드는 => 비동기 처리가 반드시 필요함.
+//                        비동기 : 다른 할 일들을 하다가, 완료 되면 별도로 실행해주자.
+//                        OkHttp : 비동기 처리를 자동으로 지원. => 별도 쓰레드가 알아서 진행.
+//                         => 이 onResponse는 다른 쓰레드가 돌리고 있다.
+//                        UI동작은 메인쓰레드가 전용으로 처리함.
+//                         -> 다른쓰레드가 UI를 건드리면 앱이 터짐.
+
+                        Log.d("JSON내용-메인에서", json.toString());
+
+                        try {
+                            final String message = json.getString("message");
+                            Log.d("서버가주는 메세지", message);
+
+                            int code = json.getInt("code");
+                            Log.d("서버가주는 코드값", code+"");
+
+                            if (code == 200) {
+//                                해당 기능이 성공적으로 동작
+                            }
+                            else {
+//                                뭔가 문제가 있었다.
+
+//                                Toast를 띄우는데 앱이 죽는다! => UI쓰레드가 아닌데, 토스트를 띄우니까.
+//                                조치 : UIThread 안에서 토스트를 띄우도록
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+
+
             }
         });
+
     }
 
     @Override
